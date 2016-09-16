ccf26472148a464565f882546315f43c8a33fe43
Deivydas <deivydas.voroneckis@cern.ch>
2016-09-16 23:20:24 +0200

    Everything works cool.

--- query_database.py
+++ query_database.py
@@ -14,25 +14,20 @@ from sqlalchemy.sql import and_, or_, not_
 import ctypes
 
 def read_data(conn, values, path, run, luminosity):
-    labels = [True]
-    lumisections = []
-    properties = [] #16
-    for i in range(0, 18):
+    properties = []
+    for i in range(0, 20):
         properties.append([])
-
-    property_name = ['PATH', 'RUN_NUMBER', 'LUMISECTION', 'ENTRIES', 'X_MEAN', 'X_MEAN_ERROR', 'X_RMS', 'X_RMS_ERROR',
-                     'X_UNDERFLOW', 'X_OVERFLOW', 'Y_MEAN', 'Y_MEAN_ERROR', 'Y_RMS', 'Y_RMS_ERROR', 'Y_UNDERFLOW',
-                     'Y_OVERFLOW', 'Z_MEAN', 'Z_MEAN_ERROR', 'Z_RMS', 'Z_RMS_ERROR', 'Z_UNDERFLOW', 'Z_OVERFLOW']
 
     def set_properties(stm):
         rs = conn.execute(stm).fetchall()
         for row in rs:
-            lumisections.append(row[2])
-            for property in range(0, 18):
-                properties[property].append(row[property + 4])
+            properties[0].append(row[0])
+            properties[1].append(row[2])
+            for i in range(2, 20):
+                properties[i].append(row[i-2 + 4])
 
     try:
-        if (path and luminosity and run>=0):
+        if (path and luminosity and run >= 0):
             print "QUERY: SELECT * FROM HISTOGRAM_VALUES WHERE LUMISECTION =", luminosity, "AND RUN_NUMBER =", run, "AND PATH =", path
             stm = select([values]).where(and_(values.c.PATH == path, values.c.LUMISECTION == luminosity, values.c.RUN_NUMBER == run))
             set_properties(stm)
@@ -66,20 +61,30 @@ def read_data(conn, values, path, run, luminosity):
             sys.exit(2)
         else:
             print "Properties: :" + properties.__str__() + '\n'
+            return properties
 
     except e:
         print "ERROR: An error occurred:", e
 
+
+def show_data(properties):
+    property_name = ['PATH', 'LUMISECTION', 'X_MEAN', 'X_MEAN_ERROR', 'X_RMS', 'X_RMS_ERROR', 'X_UNDERFLOW',
+                     'X_OVERFLOW', 'Y_MEAN', 'Y_MEAN_ERROR', 'Y_RMS', 'Y_RMS_ERROR', 'Y_UNDERFLOW','Y_OVERFLOW',
+                     'Z_MEAN', 'Z_MEAN_ERROR', 'Z_RMS', 'Z_RMS_ERROR', 'Z_UNDERFLOW', 'Z_OVERFLOW']
+
+    show_data.isLabeled = True
+
     fig = plt.figure(figsize=(14, 8))
     fig.canvas.set_window_title('Histogram values')
     ax = fig.add_subplot(111)
-    plt.xticks(lumisections)
-    x = lumisections
-    y = properties[0]
+    plt.xticks(properties[1])
+
+    x = properties[1]
+    y = properties[2]
     nr = 0
-    for prop in properties:
+    for prop in properties[2:]:
         if (prop[0]!=0.0 and prop[1]!=0.0):
-            nr+=1
+            nr += 1
 
     plot1, = ax.plot( x, y, 'ro', picker=5)
 
@@ -89,6 +94,7 @@ def read_data(conn, values, path, run, luminosity):
     for xy in zip(x, y):
         read_data.ano.append(ax.annotate(' (%s)' % xy[1], xy=xy, textcoords='data'))
 
+    path = properties[0][0]
     plt.xlabel('Luminosity')
     plt.title(path + '\n' + property_name[4])
     plt.grid()
@@ -99,51 +105,51 @@ def read_data(conn, values, path, run, luminosity):
     sfreq = Slider(axfreq, 'Property', 1, nr, valinit=1, valfmt='%0.0f')
 
     def update(val, remove = True):
-        if (remove):
+        if (remove and len(read_data.ano)):
             [a.remove() for a in read_data.ano]
-        read_data.ano[:] = []
-        y = properties[int(round(val)) - 1]
+            del read_data.ano[:]
+        y = properties[int(round(val))+1]
         plot1.set_ydata(y)
         ax.set_title(path + '\n' + property_name[int(round(val)) + 3])
         ax.set_ylim([min(y) - 0.0001, max(y) + 0.0001])
-        if labels[0]:
+        if show_data.isLabeled:
             for xy in zip(x, y):
                 read_data.ano.append(ax.annotate(' (%s)' % xy[1], xy=xy, textcoords='data'))
         draw()
 
+    sfreq.on_changed(update)
 
+    selected = []
     def onpick(event):
         thisline = event.artist
         xdata = thisline.get_xdata()
         ydata = np.asarray(thisline.get_ydata())
-        ind = event.ind
-        points = tuple(zip(xdata[ind], ydata[ind]))
-        print points[0][1]
+        points = tuple(zip(xdata[event.ind], ydata[event.ind]))
+        str = ' (%s)' % points[0][1]
+        for s in read_data.ano:
+            if s._text == str and s._x == points[0][0]:
+                s.remove()
+                read_data.ano.remove(s)
+                show()
+                return
+        selected.append(points[0][1])
         read_data.ano.append(ax.annotate(' (%s)' % points[0][1], xy=points[0], textcoords='data'))
-        update(sfreq.val, labels[0])
-        print('onpick points:', points)
-
+        update(sfreq.val, show_data.isLabeled)
 
     fig.canvas.mpl_connect('pick_event', onpick)
 
-    sfreq.on_changed(update)
-
     rax = plt.axes([0.905, 0.5, 0.085, 0.1])
-    check2 = CheckButtons(rax, ('Label', ), (True,))
+    check = CheckButtons(rax, ('Label', ), (True,))
 
-    def colorfunc(label):
-        if labels[0]:
-            [a.remove() for a in read_data.ano]
-            labels[0] = False
-            draw()
+    def label_click(label):
+        if show_data.isLabeled:
+            show_data.isLabeled = False
+            update(sfreq.val, True)
         else:
-            labels[0] = True
+            show_data.isLabeled = True
             update(sfreq.val, False)
 
-            #update2()
-        print 'hi'
-
-    check2.on_clicked(colorfunc)
+    check.on_clicked(label_click)
 
     show()
 
@@ -186,8 +192,8 @@ if __name__ == "__main__":
     meta = MetaData(eng)
     values = Table('histogram_values', meta, autoload=True)
 
-    read_data(conn, values, args.path, args.run, args.luminosity)
+    properties = read_data(conn, values, args.path, args.run, args.luminosity)
 
-    show_data(lumisections, properties)
+    show_data(properties)
 
 
